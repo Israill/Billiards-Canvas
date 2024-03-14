2e93bcbb988320df6f2002eaed913eee3341afea
Israill <cosynap@mail.ru>
2024-03-15 02:49:12 +0300

    test task canvas react ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BilliardsGame from './BilliardsGame';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Billiards Game</h1>
+      <BilliardsGame />
+    </div>
+  );
+};
+
+export default App;
